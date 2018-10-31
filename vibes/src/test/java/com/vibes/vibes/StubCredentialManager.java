92abd970ccdccc13bdab919bc80ddd30203fce88
Peter Compernolle <peter.compernolle@vibes.com>
2018-10-31 14:32:02 -0500

    2.0.02

---
+++ vibes/src/test/java/com/vibes/vibes/StubCredentialManager.java
@@ -0,0 +1,21 @@
+package com.vibes.vibes;
+
+class StubCredentialManager implements CredentialManagerInterface {
+    private Credential credential;
+
+    public StubCredentialManager() {}
+
+    public StubCredentialManager(Credential credential) {
+        this.credential = credential;
+    }
+
+    @Override
+    public void setCurrent(Credential credential) {
+        this.credential = credential;
+    }
+
+    @Override
+    public Credential getCurrent() {
+        return credential;
+    }
+}
